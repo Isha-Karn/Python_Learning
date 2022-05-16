d6aab0fbae0eaaa060e19d74afd2f2ccfe08a375
Isha <ishakarn9@gmail.com>
2022-05-16 15:57:55 +0545

    This is currency converter of Nepal

---
+++ currency_converter.py
@@ -0,0 +1,13 @@
+with open("currencyData.txt") as f:
+ 	lines= f.readlines()
+
+currencyDict={}
+for line in lines:
+	parsed= line.split("\t")
+	currencyDict[parsed[0]]= parsed[1]
+
+amount= int(input("Enter the amount: "))
+print("Choose the currency you want to convert. The available options are:")
+[print(item) for item in currencyDict.keys()]
+currency= input("choose one of these: ")
+print(f"{amount} NPR is equal to {amount*float(currencyDict[currency])} {currency}")
