00e40f3a5611b1a154aae29f28c869f327499bcd
isha <ishakarn9@gmail.com>
2022-05-18 00:02:29 +0545

    made function

--- Self_Practise/expenses_pr_01.py
+++ Self_Practise/expenses_pr_01.py
@@ -1,15 +1,33 @@
-list= []
-while(True):
-    item= input("Enter what items did you buy?")
-    quan= (input("Enter the amount of the item: "))
-    cost= int(input("Enter the cost of the item: "))
-    choice  =   input("Do you want to add more items? ")
-    list.append([item, quan, cost])
-    if choice== "n":
-        with open("expenses.txt","a")as f:
-            for i in list:
-                f.write(f" bought {i[0]} {i[1]} at Rs{i[2]}\n ")
-                print(f" bought {i[0]} {i[1]} at Rs{i[2]}\n ")
-                # [f.write(f" bought {i[0]} {i[1]} at Rs{i[2]}\n ") for i in list]--> list comprehension
+def add():
+    list= []
+    while(True):
+        item= input("Enter what items did you buy?")
+        quan= (input("Enter the amount of the item: "))
+        cost= int(input("Enter the cost of the item: "))
+        choice  =   input("Do you want to add more items? ")
+        list.append([item, quan, cost])
+        if choice== "n":
+            with open("expenses.txt","a")as f:
+                for i in list:
+                    f.write(f" bought {i[0]} {i[1]} at Rs{i[2]}\n")
+                    print(f" bought {i[0]} {i[1]} at Rs{i[2]}")
+                    # [f.write(f" bought {i[0]} {i[1]} at Rs{i[2]}\n ") for i in list]--> list comprehension
+            
+            break
+
+def view():
+    with open ("expenses.txt")as f:
+        a= f.readlines()
+        [print (i) for i in a]
+
+def main():
+    print("Press 1 to add data or press 2 view data")
+    a= int(input("Enter your choice: "))
+    if a== 1:
+        add()
+    else:
+        view()
+
         
-        break
+main()
+
